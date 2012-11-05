40e91e4d5066fb83b20c71932ebc045a49f9d907
Paweł Pacana <pawel.pacana@gmail.com>
2012-11-05 22:31:38 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WrocLove::Application.config.secret_token = 'ee32a196de2636ca2fe7f6ef5f4c5a6e31af5be750c30f51fef74607fd441b11e10a71cbb1cf6623a22dec672e0944a54081bd5928098903a101eed3e32aa851'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+WrocLove::Application.config.session_store :cookie_store, key: '_wroc_love_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# WrocLove::Application.config.session_store :active_record_store
