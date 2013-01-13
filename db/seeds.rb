54bb80fb38158aa4b2874f050e10e0d705d45808
Mateusz Lenik <mt.lenik@gmail.com>
2013-01-13 19:52:21 +0100

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,13 +2,16 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
 filename = File.expand_path("../seeds.yml", __FILE__)
-venues   = YAML.load_file(filename)
+seeds    = YAML.load_file(filename)
 
-venues.each do |v|
+seeds.fetch(:venues).each do |v|
   venue    = Venue.find_or_create_by_name v.fetch(:name)
   location = Location.new v.fetch(:latitude), v.fetch(:longtitude)
-  venue.update_attributes :address => v.fetch(:address), :location => location
-
+  # begin
+    venue.update_attributes :address => v.fetch(:address), :location => location
+  # rescue Exception
+    # binding.pry
+  # end
   v.fetch(:slots).each do |s|
     slot = Slot.find_or_create_by_name s.fetch(:name)
     slot.update_attributes :starting_at => s.fetch(:starting_at), :ending_at => s.fetch(:ending_at),
@@ -28,4 +31,11 @@ venues.each do |v|
   end
   venue.save!
 end
+
+seeds.fetch(:supporters).each do |s|
+  Supporter.create! :logo_url    => s.fetch(:logo_url),
+                    :name        => s.fetch(:name),
+                    :website_url => s.fetch(:url)
+end
+
 
